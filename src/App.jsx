17e56dc595fd3679b0402067a80c764e3eef78ad
ngwasidwan <sidwanche@gmail.com>
2025-01-01 18:40:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
+
+function App() {
+  const [scan, setScan] = useState("");
+  function handleScan(data) {
+    console.log(data);
+    setScan(data);
+  }
+  function handleError(error) {
+    setScan("error");
+    console.log(error);
+  }
+  return (
+    <div>
+      {scan && <p>{scan}</p>}
+
+      <Scanner onScan={handleScan} onError={handleError} scanDelay={300} />
+
+      <p>new scanner</p>
+    </div>
+  );
+}
+
+export default App;
