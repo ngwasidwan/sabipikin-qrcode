e410f3a9f590273bba28f97dd118ff92923e54b5
ngwasidwan <sidwanche@gmail.com>
2025-01-02 19:08:45 +0100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import { Scanner } from "@yudiel/react-qr-scanner";
-import { useState } from "react";
-// import QRCode from "react-qr-code";
 
 function App() {
-  const [scanData, setScan] = useState("");
   function handleScan(result) {
-    console.log(result);
-    alert(`data is ${result}`);
+    alert(`data is ${Object.values(result).join(" ")}`);
   }
   function handleError(error) {
-    setScan(error.message);
+    console.log(error.message);
   }
   return (
     <div>
-      {scanData && (
-        <div>
-          <p>Result: {scanData}</p>
-          <p>after scanData</p>
-        </div>
-      )}
-      <>
-        <Scanner onScan={handleScan} onError={handleError} scanDelay={300} />
-      </>
+      <Scanner onScan={handleScan} onError={handleError} scanDelay={300} />
     </div>
   );
 }
